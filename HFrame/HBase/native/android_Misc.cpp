263046ac045052df4409449d54ffb1cb81d47bf1
hackett <23925493@qq.com>
2019-06-28 16:40:46 +0800

    no message

--- HFrame/HBase/native/android_Misc.cpp
+++
@@ -1,8 +0,0 @@
-
-
-void Logger::outputDebugString (const String& text)
-{
-    __android_log_print (ANDROID_LOG_INFO, "H", "%s", text.toUTF8().getAddress());
-}
-
-
--- HFrame/HModule/product_unlocking/product_unlocking.mm
+++
@@ -1,3 +0,0 @@
-
-
-#include "product_unlocking.cpp"
